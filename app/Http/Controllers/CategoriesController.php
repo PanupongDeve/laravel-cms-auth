dc9c23cd759f7278e80d8093a909c8f8dcb8039f
PanupongDeve <5635512110@psu.ac.th>
2019-05-15 22:55:45 +0700

    delete category

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -119,5 +119,10 @@ class CategoriesController extends Controller
     public function destroy($id)
     {
         //
+        $category = Category::find($id);
+        $category->delete();
+        session()->flash('success', 'Category deleted successfully');
+
+        return redirect(route('categories.index'));
     }
 }
