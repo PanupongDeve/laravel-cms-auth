2db101517d009c08c2df5c95be85691d50e46b65
PanupongDeve <5635512110@psu.ac.th>
2019-05-17 13:46:06 +0700

    Ui_posts

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/test', 'TestsController@index')->name('test');
 
 Route::resource('categories', 'CategoriesController');
+Route::resource('posts', 'PostsController');
